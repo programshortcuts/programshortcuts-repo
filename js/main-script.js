918f550df2657c2f188e45849dc0774ee9071002
tt <tt@Mac.lan>
2026-06-16 14:33:42 -0400

    main

--- js/main-script.js
+++ js/main-script.js
@@ -23,6 +23,7 @@ sideBarTopicsAs.forEach(link => {
         }
         clickedLink = true
         const anchor = e.target.closest('a');
+        console.log(anchor)
         if (!anchor) return;
         injectContent(anchor.getAttribute('href'));
         
